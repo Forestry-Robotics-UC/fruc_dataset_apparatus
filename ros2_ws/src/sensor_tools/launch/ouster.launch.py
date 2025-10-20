5447cdeb12ecd1c5bf36d2002124b9586f05a48a
Steam Deck User <mjpc13@protonmail.com>
2025-10-20 23:46:14 +0100

    Add Ouster launch files

--- ros2_ws/src/sensor_tools/launch/ouster.launch.py
+++ ros2_ws/src/sensor_tools/launch/ouster.launch.py
@@ -1,25 +1,32 @@
-"""Launch ouster nodes using a composite container"""
+# Copyright 2023 Ouster, Inc.
+#
+
+"""Launch a sensor node along with os_cloud and os_"""
 
 from pathlib import Path
 import launch
+import lifecycle_msgs.msg
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import LifecycleNode
 from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription,
-                            ExecuteProcess, TimerAction)
+                            RegisterEventHandler, EmitEvent, LogInfo)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, FindExecutable
+from launch.substitutions import LaunchConfiguration
+from launch.events import matches_action
+from launch_ros.events.lifecycle import ChangeState
+from launch_ros.event_handlers import OnStateTransition
 
 
 def generate_launch_description():
     """
-    Generate launch description for running ouster_ros components in a single
-    process/container.
+    Generate launch description for running ouster_ros components separately each
+    component will run in a separate process).
     """
     ouster_ros_pkg_dir = get_package_share_directory('ouster_ros')
+    sensor_tools_pkg_dir = get_package_share_directory('sensor_tools')
     default_params_file = \
-        Path(ouster_ros_pkg_dir) / 'config' / 'os_sensor_cloud_image_params.yaml'
+        Path(ouster_ros_pkg_dir) / 'config' / 'ouster.yaml'
     params_file = LaunchConfiguration('params_file')
     params_file_arg = DeclareLaunchArgument('params_file',
                                             default_value=str(
@@ -31,47 +38,52 @@ def generate_launch_description():
         'ouster_ns', default_value='ouster')
 
     rviz_enable = LaunchConfiguration('viz')
-    rviz_enable_arg = DeclareLaunchArgument('viz', default_value='True')
+    rviz_enable_arg = DeclareLaunchArgument('viz', default_value='False')
 
-    auto_start = LaunchConfiguration('auto_start')
-    auto_start_arg = DeclareLaunchArgument('auto_start', default_value='True')
+    os_driver_name = LaunchConfiguration('os_driver_name')
+    os_driver_name_arg = DeclareLaunchArgument(
+        'os_driver_name', default_value='os_driver')
 
-    os_sensor = ComposableNode(
+    os_driver = LifecycleNode(
         package='ouster_ros',
-        plugin='ouster_ros::OusterSensor',
-        name='os_sensor',
+        executable='os_driver',
+        name=os_driver_name,
         namespace=ouster_ns,
-        parameters=[params_file,
-        {'auto_start': auto_start}]
-    )
-
-    os_cloud = ComposableNode(
-        package='ouster_ros',
-        plugin='ouster_ros::OusterCloud',
-        name='os_cloud',
-        namespace=ouster_ns,
-        parameters=[params_file]
-    )
-
-    os_image = ComposableNode(
-        package='ouster_ros',
-        plugin='ouster_ros::OusterImage',
-        name='os_image',
-        namespace=ouster_ns,
-        parameters=[params_file]
-    )
-
-    os_container = ComposableNodeContainer(
-        name='os_container',
-        namespace=ouster_ns,
-        package='rclcpp_components',
-        executable='component_container_mt',
-        composable_node_descriptions=[
-            os_sensor,
-            os_cloud,
-            os_image
-        ],
+        parameters=[params_file],
         output='screen',
+    )
+
+    sensor_configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(os_driver),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    sensor_activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=os_driver, goal_state='inactive',
+            entities=[
+                LogInfo(msg="os_driver activating..."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(os_driver),
+                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+            handle_once=True
+        )
+    )
+
+    sensor_finalized_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=os_driver, goal_state='finalized',
+            entities=[
+                LogInfo(
+                    msg="Failed to communicate with the sensor in a timely manner."),
+                EmitEvent(event=launch.events.Shutdown(
+                    reason="Couldn't communicate with sensor"))
+            ],
+        )
     )
 
     rviz_launch_file_path = \
@@ -85,7 +97,10 @@ def generate_launch_description():
         params_file_arg,
         ouster_ns_arg,
         rviz_enable_arg,
-        auto_start_arg,
+        os_driver_name_arg,
         rviz_launch,
-        os_container
+        os_driver,
+        sensor_configure_event,
+        sensor_activate_event,
+        sensor_finalized_event
     ])
