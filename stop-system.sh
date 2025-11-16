d46c0c098426e8ce5deed345dd14dc7ac45f887a
Toby <tobias.hulland@gmail.com>
2025-11-16 17:53:52 +0000

    SIGINT recording container fix

--- stop-system.sh
+++ stop-system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+podman exec recording pkill -SIGINT -f hector_recorder # Required for graceful shutdown, because it expects a CTRL+C from interactive terminal
 podman stop recording
 podman stop monitoring 
 podman stop xsens
