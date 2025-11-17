49cae266b9293b2b02f73691684e6196e6e77667
Toby <tobias.hulland@gmail.com>
2025-11-17 19:29:41 +0000

    driver fix 2

--- ros2_ws/shared/scripts/xsens-launch.sh
+++ ros2_ws/shared/scripts/xsens-launch.sh
@@ -3,7 +3,7 @@ set -e
 
 source /docker_ws/install/setup.bash
 
-echo $(cat /ros2_ws/shared/scripts/mtdevice.py) > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtdevice.py
-echo $(cat /ros2_ws/shared/scripts/mtnode.py) > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtnode.py
+cat /ros2_ws/shared/scripts/mtdevice.py > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtdevice.py
+cat /ros2_ws/shared/scripts/mtnode.py > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtnode.py
 
-ros2 launch xsens_driver xsens_driver.launch.xml
+ros2 launch xsens_driver xsens_driver.launch.xml baudrate:=460800 device:=/dev/ttyUSB0
