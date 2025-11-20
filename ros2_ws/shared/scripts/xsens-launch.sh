83cf917669a780922940bec547ae6ce36d26f869
Toby <tobias.hulland@gmail.com>
2025-11-20 18:13:15 +0000

    imu frame topic fix

--- ros2_ws/shared/scripts/xsens-launch.sh
+++ ros2_ws/shared/scripts/xsens-launch.sh
@@ -6,4 +6,5 @@ source /docker_ws/install/setup.bash
 cat /ros2_ws/shared/scripts/mtdevice.py > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtdevice.py
 cat /ros2_ws/shared/scripts/mtnode.py > /docker_ws/src/norlab_xsens_driver/xsens_driver/mtnode.py
 
-ros2 launch xsens_driver xsens_driver.launch.xml baudrate:=460800 device:=/dev/ttyUSB0
+ros2 launch xsens_driver xsens_driver.launch.xml baudrate:=460800 device:=/dev/ttyUSB0 frame_id:=xsens_imu
+
