fa70d3ffaa247ccc1180aa9e88d3eade74b8e65b
Toby <tobias.hulland@gmail.com>
2025-12-16 19:49:36 +0000

    dev permissions fix

--- launch-system.sh
+++ launch-system.sh
@@ -18,6 +18,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Since running script as system, we can get away with preparing serial device permissions for containers to use them?
+chmod 777 /dev/ttyUSB0
+
 #SELECT TOPICS FOR RECORDING
 topics=$(kdialog --checklist "Topics to record" \
     /imu/data "Xsens IMU" on \
