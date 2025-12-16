ad9b2364f6a83a45175a83d2e7206c9c2a90fc53
Toby <tobias.hulland@gmail.com>
2025-12-16 19:50:32 +0000

    dev permissions fix

--- launch-system.sh
+++ launch-system.sh
@@ -20,6 +20,7 @@ fi
 
 # Since running script as system, we can get away with preparing serial device permissions for containers to use them?
 chmod 777 /dev/ttyUSB0
+chmod 777 /dev/ttyACM1
 
 #SELECT TOPICS FOR RECORDING
 topics=$(kdialog --checklist "Topics to record" \
